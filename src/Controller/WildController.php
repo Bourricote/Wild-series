e0718634a307e3e83028f3d9d135767d1f28dcda
Anne Quiedeville <anne.quiedeville@orange.fr>
2019-11-18 10:43:24 +0100

    Done quest Symfony-05-routing

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -7,15 +7,35 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
+/**
+ * @Route("/wild", name="wild_")
+ */
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/", name="index")
      */
-    public function index() : Response
+    public function index(): Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @Route("/show/{slug}",
+     *      requirements={"slug"="^[a-z0-9]+(?:-[a-z0-9]+)*$"},
+     *      name="show")
+     * @param string $slug
+     * @return Response
+     */
+    public function show(string $slug = ''): Response
+    {
+        if ($slug === '') {
+            $title = 'Aucune série sélectionnée, veuillez choisir une série';
+        } else {
+            $title = ucwords(str_replace('-', ' ', $slug));
+        }
+        return $this->render('wild/show.html.twig', ['title' => $title]);
     }
 }
